ae2a973b880922a37b16c07edfcc8a21d1a88c0b
Tom <twhitehead.1991@gmail.com>
2019-09-21 12:07:28 +0100

    Fixed procfile

--- tracklistify/settings_prod.py
+++ tracklistify/settings_prod.py
@@ -6,7 +6,7 @@ from .settings_common import *  # IMPORT COMMON SETTINGS
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['secret-fortress-69536.herokuapp.com']  # prod
+ALLOWED_HOSTS = ['damp-hamlet-30807.herokuapp.com']  # prod
 
 # AWS S3 bucket credentials
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
